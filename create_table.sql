02cd9035ead2b53dac99a78c1a0b50d7d11c33bb
huangyiheng <814919935@qq.com>
2019-09-23 12:29:44 +0800

    [*]补全MySQL导入数据的sql文件

--- create_table.sql
+++ create_table.sql
@@ -1,8 +1,30 @@
+/* mysql  Ver 14.14 Distrib 5.7.24, for linux-glibc2.12 (x86_64) using  EditLine wrapper */
+
+create database industry DEFAULT CHARACTER SET utf8;
+use industry;
+
 CREATE TABLE dim_industry (
-  id int,
+  id int AUTO_INCREMENT,
   industry_id varchar(12),
-  name varchar(100),
+  name varchar(1000),
   parent_id varchar(12),
   level_type int,
   description varchar(4000)
+);
+
+CREATE TABLE dim_industry2 (
+  id                        int auto_increment,
+  category_industry_id      varchar(10),
+  category_name             varchar(1000),
+  category_desc             varchar(1000),
+  large_class_industry_id   varchar(10),
+  large_class_name          varchar(1000),
+  large_class_desc          varchar(1000),
+  mid_class_industry_id     varchar(10),
+  mid_class_name            varchar(1000),
+  mid_class_desc            varchar(1000),
+  small_class_industry_id   varchar(10),
+  small_class_name          varchar(1000),
+  small_class_desc          varchar(1000),
+  primary key(id)
 );
