1b8156aa920626c0d7bf3f539ac41f0642325319
huangyiheng <814919935@qq.com>
2019-09-23 12:14:22 +0800

    [*]mysql建表语句修正

--- create_table.sql
+++ create_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dim_industry (
-  id number,
+  id int,
   industry_id varchar(12),
   name varchar(100),
   parent_id varchar(12),
-  level_type number,
+  level_type int,
   description varchar(4000)
 );
